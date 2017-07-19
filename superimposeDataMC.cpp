b83eea561a1bffa0ec6291a897fa30ac5fab41eb
Joe Singh <joesingh@nino.physics.berkeley.edu>
2017-07-19 13:04:20 -0700

    Added mean follower superimposing

--- superimposeDataMC.cpp
+++ superimposeDataMC.cpp
@@ -15,8 +15,8 @@ void plot(TCanvas* canv, int num, std::string name, TFile* dataFile, TFile* mcFi
 
     histMC->SetFillColor(kRed);
     histMC->SetFillStyle(3001);
-    histData->Draw("e1");
-    histMC->Draw("e2 same");
+    histMC->Draw("e2");
+    histData->Draw("e1 same");
 }
 
 void superimposeDataMC(std::string nameMC, std::string nameDATA)
@@ -29,6 +29,11 @@ void superimposeDataMC(std::string nameMC, std::string nameDATA)
     cprompts->Divide(3, 1);
     plot(cprompts, 1, "hprompt_eeffenergy", dataFile, mcFile);
     plot(cprompts, 2, "hprompt_ueffenergy", dataFile, mcFile);
-    plot(cprompts, 3, "hprompt_meffenergy", dataFile, mcFile);
+    plot(cprompts, 3, "hprompt_meffenergy", dataFile, mcFile); 
 
+    TCanvas* cnfollowersmean_eeffenergy = new TCanvas("cnfollowersmean_eeffenergy", "Number of Followers vs Prompt Energy", 1600, 800);
+    cnfollowersmean_eeffenergy->Divide(3, 1);
+    plot(cnfollowersmean_eeffenergy, 1, "nfollowersmean_eeffenergy", dataFile, mcFile);
+    plot(cnfollowersmean_eeffenergy, 2, "nfollowersmean_sr_eeffenergy", dataFile, mcFile);
+    plot(cnfollowersmean_eeffenergy, 3, "nfollowersmean_mr_eeffenergy", dataFile, mcFile);
 }
